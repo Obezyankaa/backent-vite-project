2717da51c33e5438536599715596803f69f93919
Vladislav Voloshanovskiy <voloshanowky@yandex.ru>
2024-09-11 20:36:27 +0400

    start express

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import path from "path";
+console.log(123);
+
+const app = express();
+const port = 3000;
+
+const __dirname = path.resolve();
+
+// Логирование всех входящих запросов
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next(); // передаем управление следующему middleware или маршруту
+});
+
+// Указываем Express раздавать файлы из папки dist
+app.use(express.static(path.join(__dirname, "dist")));
+
+// Статические файлы для PDF (если они есть)
+app.use("/pdf-files", express.static(path.join(__dirname, "pdf-files")));
+
+// Все остальные запросы направляем на индексный файл React приложения
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
